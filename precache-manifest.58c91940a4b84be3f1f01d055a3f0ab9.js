1067ab9e37662c1513a310e51297485e637be545
Ian Canupp <canuppian@gmail.com>
2020-08-06 13:18:23 -0400

    Updates

---
+++ precache-manifest.58c91940a4b84be3f1f01d055a3f0ab9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c97d853e033e6e6526f3c420415a2673",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "30fac2317babe2a50fea",
+    "url": "/portfolio/static/css/main.c9ef6361.chunk.css"
+  },
+  {
+    "revision": "46f3529a97abb1e88cc5",
+    "url": "/portfolio/static/js/2.20a1355e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.20a1355e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30fac2317babe2a50fea",
+    "url": "/portfolio/static/js/main.ae72ad48.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
